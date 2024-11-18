2c44dfe08e60a1858e0fa0b68dd38e07214ee49e
“Sagarpatil8421” <sagarpatil842000@gmail.com>
2024-11-18 13:06:45 +0530

    New UI

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './LandingPage.css'; // Custom CSS for styling
+import './LandingPage.css'; 
+import { useAuth } from '../auth/AuthContext';
 import ImageSlider from './ImageSlider';
 import { colors } from '@mui/material';
 
+
 const LandingPage = () => {
+  // Check if the user is logged in
+  const { currentUser } = useAuth(); 
+  const isLoggedIn = Boolean(currentUser);
+
+  const handleContactPopup = () => {
+    alert('Contact us here for detailed information and exciting offers!');
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className="landing-page">
       {/* Hero Section */}
@@ -12,7 +23,14 @@ const LandingPage = () => {
         <div class="hero-container">
             <h2>Welcome to<b style={{color:'#ff6b6b'}}> MY FITNESS </b></h2>
             <p>Your fitness journey starts here. Let us help you reach your goals.</p>
-            <Link to="/signup" className="cta-btn">Sign Up Now</Link>
+            {isLoggedIn ? (
+              <button className="cta-btn" onClick={handleContactPopup}>
+                Let’s Get Moving!
+              </button>
+            ):(
+              <Link to="/signup" className="cta-btn">Join Us Today</Link>
+            )}
+
         </div>
       </section>
 
@@ -47,7 +65,13 @@ const LandingPage = () => {
       
       <div className="call-to-action">
         <h2>Ready to join?</h2>
-        <Link to="/signup" className="cta-btn">Sign Up Now</Link>
+        {isLoggedIn ? (
+          <button className="cta-btn" onClick={handleContactPopup}>
+            Let’s Get Moving!
+          </button>
+        ):(
+          <Link to="/signup" className="cta-btn">Join Us Today</Link>
+        )}
       </div>
       <section id="contact">
           <div class="container">
